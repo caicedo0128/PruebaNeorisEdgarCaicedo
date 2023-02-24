45d8726bcc23f2e9da55dd53e0b51c896f4d368d
Stephanie Martinez Muñoz <caicedo.edg@gmail.com>
2023-02-24 12:21:31 -0500

    Creacion repo prueba Neoris

---
+++ src/main/java/com/pruebaNeoris/pruebaNeoris/PruebaNeorisApplication.java
@@ -0,0 +1,13 @@
+package com.pruebaNeoris.pruebaNeoris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaNeorisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaNeorisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pruebaNeoris/pruebaNeoris/PruebaNeorisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebaNeoris.pruebaNeoris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaNeorisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
